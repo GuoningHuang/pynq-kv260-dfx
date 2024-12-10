00e4cbbbfa5cd6fe93c2d54bebf747a59418c15c
GuoningHuang <75004653+GuoningHuang@users.noreply.github.com>
2024-12-10 16:51:01 +0800

    Add files via upload

---
+++ HLS/sub.cpp
@@ -0,0 +1,13 @@
+
+
+void sub(int a,int b ,int& c);
+
+void sub(int a,int b,int& c){
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE s_axilite port=a
+#pragma HLS INTERFACE s_axilite port=b
+#pragma HLS INTERFACE s_axilite port=c
+	c = a - b;
+}
+
+
